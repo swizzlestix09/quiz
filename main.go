a418d237dc232f46ddb9f6e03e609935f9db9747
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-17 22:02:11 -0500

    csv parser implemented

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
 type Questions struct {
 	Question string
 	Answer   string
 }
 
-func createQuiz() {
-	//read csv
-	//store in a list
+func createQuizList(data [][]string) {
+	fmt.Printf("%+v\n", data)
 	//each line in its own array - 0 is q 1 is ans
 	//returning
+	return
 }
 func main() {
+	file, err := os.Open("problems.csv")
+	//read csv
+	if err != nil {
+		log.Fatal("error opening csv:", err)
+	}
+	defer file.Close()
+	//store in a list
+	fileReader := csv.NewReader(file)
+	data, err := fileReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//attain csv file info
+	// questionsList := createQuizList(data)
+	createQuizList(data)
 
+	// fmt.Printf("%+v\n", questionsList)
 }
